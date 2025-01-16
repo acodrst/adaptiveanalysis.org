09647bd663ed815e3530049c1dec8bf12abb9f0f
ORNG <acodrst@mail.com>
2025-01-16 14:56:58 -0800

    intro and latex

--- assets/wrap.lua
+++ assets/wrap.lua
@@ -1,16 +1,15 @@
 function wrap(el,side)
-    w=string.gsub(el.content[1].content[1].attributes.width,'px','')
-    h=string.gsub(el.content[1].content[1].attributes.height,'px','')
-    l=tostring(math.floor(h/96/.233+3))
+    s=el.content[1].content[1].attributes.s
 return {
-       pandoc.RawInline('latex',' \\begin{wrapfigure}['..l..']{'..side..'}{'..tostring(tonumber(w)/96+.2)..'in}'),
-       pandoc.RawInline('latex','\\centering \\includesvg[width='..tostring(tonumber(w)/96)..'in]{'..el.content[1].content[1].src..'}'),
+       pandoc.RawInline('latex',' \\begin{wrapfigure}[]{'..side..'}{0px}'),
+       pandoc.RawInline('latex','\\centering \\includesvg[scale='..s..']{'..el.content[1].content[1].src..'}'),
        pandoc.RawInline('latex','\\caption{'..el.caption.long[1].content[1].text..'}\\label{'..el.identifier..'}'),
        pandoc.RawInline('latex',' \\end{wrapfigure}')
 }
 end
 
 function Figure(el)
+  print(el.content[1].content[1])
   if el.content[1].content[1].src=='images/Top.svg'
   then 
     return wrap(el,'o')
